ebc75a86e3602cab2f3a0a548c15606c2088fc88
clonw <paquitol@gmail.com>
2018-02-11 22:10:33 +0100

    Capitulo 76

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
+import { User } from '../../models/user';
+import {UserService } from '../../services/user.service';
+
 
 @Component({
     selector: 'login',
@@ -7,15 +10,22 @@ import { Router, ActivatedRoute, Params} from '@angular/router';
 })
 export class LoginComponent implements OnInit{
     public title: String;
+    public user: User;
 
     constructor(
         private _route: ActivatedRoute,
         private _router: Router
     ){
         this.title = 'Indentificate';
+        this.user = new User('', '', '', '', '', 'ROLE_USER', '');
     }
 
     ngOnInit(){
         console.log('login.component cargado!!');
     }
+
+    onSubmit(){
+        console.log( this.user);
+    }
+
 }
