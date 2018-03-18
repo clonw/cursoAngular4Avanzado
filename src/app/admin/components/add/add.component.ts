728842b5abc205afc73a28695a66a4765f289616
clonw <paquitol@gmail.com>
2018-03-18 13:28:11 +0100

    Capitulo 88

--- src/app/admin/components/add/add.component.ts
+++ src/app/admin/components/add/add.component.ts
@@ -1,10 +1,41 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
+
+import { GLOBAL } from '../../../services/global';
+import { Animal } from '../../../models/animal';
+import { AnimalService} from '../../../services/animal.service';
+import { UserService} from '../../../services/user.service';
+import { UploadService} from '../../../services/upload.service';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'admin-add',
   templateUrl: './add.component.html',
+  providers: [UserService, AnimalService, UploadService]
 })
-export class AddComponent{
-  title = 'Añadir';
+export class AddComponent implements OnInit{
+  public title = 'Añadir';
 
+  public animal: Animal;
+  public identity;
+  public token;
+  public url: string;
+
+  constructor(
+
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService,
+    private _uploadService: UploadService
+  ){
+    this.title = 'Añadir';
+    this.animal = new Animal('', '', 2017, '', '');
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.url = GLOBAL.url;
+  }
+
+  ngOnInit(){
+    console.log('animal -addc componente ha sido cargado !!');
+  }
 }
---
+++ src/app/services/animal.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+/** esto nos va a dar un método que nos permite recoger los valores que da una peticion ajax */
+import 'rxjs/add/operator/map';
+import { Observable} from 'rxjs/Observable';
+import { GLOBAL} from './global';
+
+/**Para poder inyectar el servicio dentro de componente y dentro de diferentes sitios */
+@Injectable()
+export class AnimalService{
+    public url: string;
+
+    constructor(private _http: Http){
+        this.url = GLOBAL.url;
+    }
+
+
+    addAnimal( token, animal){
+        const params = JSON.stringify(animal);
+        const headers = new Headers({
+            'Content-Type' : 'application/json',
+            'Authorization': token
+        });
+
+        return this._http.post(this.url + 'animal', params, {headers : headers})
+                .map(res => res.json);
+    }
+}
